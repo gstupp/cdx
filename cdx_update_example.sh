cc84068abbaf042ac9acc3d6b4ff5230516f98de
gstupp <gideon.stupp@gmail.com>
2016-11-01 10:49:34 +0200

    Simplify

--- cdx_update_example.sh
+++ cdx_update_example.sh
@@ -7,16 +7,6 @@ find ${HOME}/ -not -path '*/\.*' -type d | tee -a ${tmpfile} | wc -l
 echo -n "proj-*.."
 find ${HOME}/kfw/proj-* -not -path '*/\.*' -type d | tee -a ${tmpfile} | wc -l
 
-tmp=0
-echo -n "KFW workspaces..."
-for d in  ~/kfw ~/kfw/*FP* ~/kfw/*FP*/* ~/kfw/*FP*/results/* ~/kfw/*FP*/configuration/*; do
-    if [ -d "$d" ]; then
-	echo "$d" 
-	(( tmp+=1 ))
-    fi
-done >> ${tmpfile}
-echo $tmp
-
 echo "writing to ${CDX_FILE}..."
 sort -u ${tmpfile} > "${CDX_FILE}" # Important for uniqueness
 rm "$tmpfile"
