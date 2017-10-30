b84d87d3bf844eb9d0229442811ed981517fed00
Gideon Stupp <gideon.stupp@intel.com>
2017-10-30 15:25:03 +0200

    add completion function to cdx

--- cdx.d
+++ cdx.d
@@ -1,12 +1,35 @@
 CDX_MANUAL_FILE="${HOME}/.cdx_manual"
 export CDX_FILE="${HOME}/.cdx_db"
 
+function _cdx () 
+{ local cur
+  local res
+  local g
+  COMPREPLY=()   # Array variable storing the possible completions.
+  cur=${COMP_WORDS[COMP_CWORD]}
+  # Remove last "/" which Bash may have automatically added
+  if [[ $cur == */ ]]; then
+      g="${cur::${#cur}-1}"
+  else
+      g="${cur}"
+  fi
+  res="$(cdext -ip ${PWD} $g ${CDX_FILE} ${CDX_MANUAL_FILE})"
+  if [ "$res" != "$g" ] ; then 
+      COMPREPLY=(${res});
+  else
+      COMPREPLY=( $(compgen -d -- "$cur") )
+  fi
+  return 0
+}
+
 function cdx ()
 {   local ndir
     if [[ -n "${@}" ]]; then
 	ndir="$(cdext -ip ${PWD} ${@} ${CDX_FILE} ${CDX_MANUAL_FILE})"
-	if [ -n "${ndir}" ]; then
+	if [ -d "${ndir}" ]; then
             cd "${ndir}" && echo "${PWD}"
+	elif [ -d "${@}" ]; then
+	    cd "${@}" && echo "${PWD}"
 	fi
     fi
 }
@@ -32,3 +55,5 @@ function cdxa ()
 {
     echo "${PWD}" >> $CDX_MANUAL_FILE
 }
+
+complete -F _cdx -o filenames -o nospace -o bashdefault cdx
